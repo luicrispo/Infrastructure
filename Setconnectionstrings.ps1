53a83b9a8350be8ef2619d661d789e71ec71f879
luicrispo <57984522+luicrispo@users.noreply.github.com>
2019-11-26 01:32:54 +0400

    Add files via upload

---
+++ Setconnectionstrings.ps1
@@ -0,0 +1,31 @@
+﻿$mResourceGroup = $env:ResourceGroup
+
+$mySite = $env:backendname
+
+$myDb = $env:sqlservername
+
+$myDbAdmin = $env:sqladminusername
+
+$myAdminPassword = $env:sqladminpassword
+
+$connetctionstring = "Server=tcp:" + $myDb + ".database.windows.net,1433;Initial Catalog=RealWorld;Persist Security Info=False;User ID=" + $myDbAdmin + ";Password=" + $myAdminPassword + ";MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+
+$webApp = Get-AzureRMWebAppSlot -ResourceGroupName $myResourceGroup -Name $mySite -Slot production
+
+$appSettingList = $webApp.SiteConfig.ConnectionStrings
+
+$hash = @{}
+
+ForEach ($kvp in $appSettingList) {
+
+$hash[$kvp.Name] = $kvp.Value
+
+}
+
+$hash['BEDatabase'] = "$Env:Release_ReleaseId"
+
+
+Set-AzureRMWebAppSlot -ResourceGroupName $myResourceGroup -Name $mySite -ConnectionStrings $hash -Slot production
+
+Write-Host "Done!"
